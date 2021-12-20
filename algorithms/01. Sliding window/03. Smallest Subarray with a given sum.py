f39c024b5f0ca17f57adeeb8edd9774428a73c8d
Clarence San <clarencesan@gmail.com>
2021-12-20 12:11:41 +0800

    PS2

---
+++ algorithms/01. Sliding window/03. Smallest Subarray with a given sum.py
@@ -0,0 +1,56 @@
+'''
+Problem Statement
+Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’.
+Return 0, if no such subarray exists.
+
+Example 1:
+
+Input: [2, 1, 5, 2, 3, 2], S=7
+Output: 2
+Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+Example 2:
+
+Input: [2, 1, 5, 2, 8], S=7
+Output: 1
+Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
+Example 3:
+
+Input: [3, 4, 1, 1, 6], S=8
+Output: 3
+Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
+'''
+
+# implementation
+import math
+
+def smallest_subarray_with_given_sum(s, arr):
+    window_sum, window_start = 0, 0
+    min_length = math.inf
+
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end] # add the next element
+        # shrink the window as small as possible until 'window_sum' is smaller than 's'
+        while window_sum >= s:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    return min_length if min_length != math.inf else 0
+
+
+def main():
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2]))) # 2
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 8]))) # 1
+    print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6]))) # 3
+
+main()
+
+'''
+Time Complexity 
+The time complexity of the above algorithm will be O(N). 
+The outer for loop runs for all elements and the inner while loop processes each element only once, therefore the time complexity of the algorithm will be O(N+N) which is asymptotically equivalent to O(N).
+
+Space Complexity 
+The algorithm runs in constant space O(1).
+'''
