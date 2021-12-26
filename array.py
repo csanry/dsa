da688c466e713a89bc0d56f08c3d00007c81c335
Clarence San <clarencesan@gmail.com>
2021-12-26 09:15:30 +0800

    array

---
+++ array.py
@@ -0,0 +1,16 @@
+# A Python list is built like an array - elements laid out sequentially in memory
+arr = [36, 9, 61, 4, 100, 75, 74, 89, 57, 23, 94, 8, 50, 99, 68, 51, 37, 55, 6, 59]
+
+# Searching for an element O(n)
+
+
+
+# Inserting an element O(n)
+arr.insert()
+
+
+# Deleting an element O(n)
+
+# Access an element O(1)
+
+
