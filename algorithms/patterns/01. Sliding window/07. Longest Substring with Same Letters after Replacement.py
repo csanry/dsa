8faf84c51f84767775a314b46d4cce67679c1e69
Clarence San <clarencesan@gmail.com>
2022-01-07 09:05:00 +0800

    Longest substring problem

---
+++ algorithms/patterns/01. Sliding window/07. Longest Substring with Same Letters after Replacement.py
@@ -0,0 +1,57 @@
+'''
+Problem Statement
+Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
+
+Example 1: Input: String="aabccbb", k=2
+Output: 5
+Explanation: Replace the two 'c' with 'b' to have a longest repeating substring "bbbbb".
+
+Example 2:
+Input: String="abbcb", k=1
+Output: 4
+Explanation: Replace the 'c' with 'b' to have a longest repeating substring "bbbb".
+
+Example 3:
+Input: String="abccde", k=1
+Output: 3
+Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+'''
+
+# implementation
+def length_of_longest_substring(st, k):
+    window_start, max_length, max_repeat_letter_count = 0, 0, 0
+    frequency_map = {}
+
+    # try to extend the range [window_start, window_end]
+    for window_end in range(len(st)):
+        right_char = st[window_end]
+        if right_char not in frequency_map:
+            frequency_map[right_char] = 0
+        frequency_map[right_char] += 1
+        max_repeat_letter_count = max(max_repeat_letter_count, frequency_map[right_char])
+
+        # current window is from window_start to window_end, overall we have a letter which is
+        # repeating max_repeat_letter_count times, this means we should replace the remaining letters
+        # in the window. If the remaining letters are more than k, we need to shrink the window as
+        # we are not allowed to replace more than k times
+        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+            left_char = st[window_start]
+            frequency_map[left_char] -= 1
+            window_start += 1
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
+
+def main():
+    print(length_of_longest_substring("aabccbb", 2)) # 5
+    print(length_of_longest_substring("abbcb", 1)) # 4
+    print(length_of_longest_substring("abccde", 1)) # 3
+
+main()
+
+'''
+Time Complexity 
+The time complexity of the above algorithm will be O(N) where ‘N’ is the number of letters in the input string.
+
+Space Complexity 
+As we are expecting only the lower case letters in the input string, we can conclude that the space complexity will be O(26), to store each letter’s frequency in the HashMap, which is asymptotically equal to O(1).
+'''
---
+++ algorithms/patterns/07. Tree BFS/mindepth.py
@@ -0,0 +1,61 @@
+'''
+Problem Statement
+Find the minimum depth of a binary tree.
+The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+'''
+
+# implementation
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+def find_minimum_depth(root):
+    # exception case
+    if root is None:
+        return 0
+
+    queue = deque()
+    queue.append(root)
+    minimum_tree_depth = 0
+    while queue:
+        minimum_tree_depth += 1
+        level_size = len(queue)
+        for _ in range(level_size):
+            current_node = queue.popleft()
+
+            # check if this is a leaf node
+            if not current_node.left and not current_node.right:
+                return minimum_tree_depth
+
+            # insert the children of current node if not leaf node
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print(f"Tree Minimum Depth: {find_minimum_depth(root)}") # 2
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print(f"Tree Minimum Depth: {find_minimum_depth(root)}") # 3
+
+main()
+
+'''
+Time Complexity 
+The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. 
+This is due to the fact that we traverse each node once.
+
+Space Complexity 
+The space complexity of the above algorithm will be O(N) which is required for the queue. 
+Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level), 
+therefore we will need O(N) space to store them in the queue.
+'''
