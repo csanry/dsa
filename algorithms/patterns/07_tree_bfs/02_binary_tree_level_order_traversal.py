3652929f8fe6943015686b1c60b613fb111b6118
c.san <80271352+csanry@users.noreply.github.com>
2024-06-11 20:17:15 +0800

    chore: add binary tree BFS problems

--- algorithms/patterns/07_tree_bfs/02_binary_tree_level_order_traversal.py
+++ algorithms/patterns/07_tree_bfs/02_binary_tree_level_order_traversal.py
@@ -1,13 +1,14 @@
 """
 Problem Statement
-Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, ie. the lowest level comes first.
-You should populate the values of all nodes in each level from left to right in separate sub-arrays.
+Given a binary tree, populate an array to represent its level-by-level traversal.
+You should populate the values of all nodes of each level from left to right in separate sub-arrays.
 """
 
 # implementation
 from collections import deque
 
 
+# instantiate a Tree class
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -15,24 +16,29 @@ class TreeNode:
 
 
 def traverse(root):
-    result = deque()
+    result = []
     if root is None:
         return result
 
     queue = deque()
     queue.append(root)
+
     while queue:
         level_size = len(queue)
         current_level = []
+
         for _ in range(level_size):
             current_node = queue.popleft()
             # add the node to the current level
             current_level.append(current_node.val)
+            # insert the children of current node in the queue
             if current_node.left:
                 queue.append(current_node.left)
             if current_node.right:
                 queue.append(current_node.right)
-        result.appendleft(current_level)
+
+        result.append(current_level)
+
     return result
 
 
@@ -43,10 +49,12 @@ def main():
     root.left.left = TreeNode(9)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    print(f"Reverse level order traversal: {traverse(root)}")
+    print(f"Level order traversal: {traverse(root)}")
 
 
-main()
+if __name__ == "__main__":
+    main()  # output [[12], [7, 1], [9, 10, 5]]
+
 
 """
 Time Complexity
@@ -54,7 +62,7 @@ The time complexity of the above algorithm is O(N), where 'N' is the total numbe
 This is because we traverse each node once.
 
 Space Complexity
-The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal.
-We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level (this could only happen at the lowest level),
+The space complexity of the above algorithm will be O(N) to store a list containing the level order traversal.
+Since we can have a maximum of N/2 nodes at any level (this could only happen at the lowest level),
 We will need O(N) space to store in the queue.
 """
--- algorithms/patterns/07_tree_bfs/03_reverse_level_order_traversal.py
+++ algorithms/patterns/07_tree_bfs/03_reverse_level_order_traversal.py
@@ -1,14 +1,14 @@
 """
 Problem Statement
-Given a binary tree, populate an array to represent its level-by-level traversal.
-You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+Given a binary tree, populate an array to represent its level-by-level traversal in reverse order,
+ie. the lowest level comes first.
+You should populate the values of all nodes in each level from left to right in separate sub-arrays.
 """
 
 # implementation
 from collections import deque
 
 
-# instantiate a Tree class
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -16,7 +16,7 @@ class TreeNode:
 
 
 def traverse(root):
-    result = []
+    result = deque()
     if root is None:
         return result
 
@@ -30,12 +30,12 @@ def traverse(root):
             current_node = queue.popleft()
             # add the node to the current level
             current_level.append(current_node.val)
-            # insert the children of current node in the queue
             if current_node.left:
                 queue.append(current_node.left)
             if current_node.right:
                 queue.append(current_node.right)
-        result.append(current_level)
+        result.appendleft(current_level)
+
     return result
 
 
@@ -46,10 +46,12 @@ def main():
     root.left.left = TreeNode(9)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    print(f"Level order traversal: {traverse(root)}")
+    print(f"Reverse level order traversal: {traverse(root)}")
 
 
-main()  # output [[12], [7, 1], [9, 10, 5]]
+if __name__ == "__main__":
+    main()  # [[9, 10, 5], [7, 1], [12]]
+
 
 """
 Time Complexity
@@ -57,7 +59,9 @@ The time complexity of the above algorithm is O(N), where 'N' is the total numbe
 This is because we traverse each node once.
 
 Space Complexity
-The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal.
-We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level (this could only happen at the lowest level),
+The space complexity of the above algorithm will be O(N) as we need to return
+a list containing the level order traversal.
+We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level
+(this could only happen at the lowest level),
 We will need O(N) space to store in the queue.
 """
